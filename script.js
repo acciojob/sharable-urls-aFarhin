4ca7f8ebcfe5d0ce343ba0e07be1afebf6aeaec8
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-03-30 13:12:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // your code here
+  const button = document.getElementById("button");
+      const url = document.getElementById("url");
+      const name = document.getElementById("name");
+      const year = document.getElementById("year");
+      button.onclick = function() {
+        let urlString = "https://localhost:8080/";
+        if (name.value) {
+          urlString += `?name=${name.value}`;
+          if (year.value) {
+            urlString += `&year=${year.value}`;
+          }
+        } else if (year.value) {
+          urlString += `?year=${year.value}`;
+        }
+        url.textContent = urlString;
+      }
