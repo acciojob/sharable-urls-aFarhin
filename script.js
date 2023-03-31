613ccf2baa57035cd0ff4026e8b5a0b6fa9d0658
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-03-31 16:14:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 // your code here
-  const button = document.getElementById("button");
-      const url = document.getElementById("url");
-      const name = document.getElementById("name");
-      const year = document.getElementById("year");
-      button.onclick = function() {
-        let urlString = "https://localhost:8080/";
-        if (name.value) {
-          urlString += `?name=${name.value}`;
-          if (year.value) {
-            urlString += `&year=${year.value}`;
-          }
-        } else if (year.value) {
-          urlString += `?year=${year.value}`;
-        }
-        url.textContent = urlString;
-      }
+function generateURL() {
+			var name = document.getElementById("name").value;
+			var year = document.getElementById("year").value;
+
+			var url = "https://localhost:8080/?";
+			if (name != "") {
+				url += "name=" + name;
+				if (year != "") {
+					url += "&year=" + year;
+				}
+			}
+			else if (year != "") {
+				url += "year=" + year;
+			}
+
+			document.getElementById("url").textContent = url;
+		}
